3dca4e86f439c9fbad448d4041f62a41b4bced04
geromath <mathiams@stud.ntnu.no>
2018-03-20 18:41:44 +0100

    mostly spacing + category removal

--- shapely-child/page.php
+++ shapely-child/page.php
@@ -16,16 +16,16 @@ get_header(); ?>
 <?php $layout_class = shapely_get_layout_class(); ?>
 	<div class="row">
 		<div id="primary" class="col-md-8 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
-																<?php
-																while ( have_posts() ) :
-																	the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-																	get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page' );
 
-																	// If comments are open or we have at least one comment, load up the comment template.
-																	if ( comments_open() || get_comments_number() ) :
-																		comments_template();
-																		endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+					endif;
 
 			endwhile; // End of the loop.
 			?>
--- shapely-child/single.php
+++ shapely-child/single.php
@@ -10,23 +10,23 @@
 get_header(); ?>
 <?php $layout_class = shapely_get_layout_class(); ?>
 	<div class="row">
-		
+
 		<div id="primary" class="col-md-8 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
-																<?php
-																while ( have_posts() ) :
-																	the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-																	get_template_part( 'template-parts/content' );
+				get_template_part( 'template-parts/content' );
 
-																	// If comments are open or we have at least one comment, load up the comment template.
-																	if ( comments_open() || get_comments_number() ) :
-																		comments_template();
-																		endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+					endif;
 
 			endwhile; // End of the loop.
 			?>
 		</div><!-- #primary -->
-		
+
 	</div>
 <?php
 get_footer();
